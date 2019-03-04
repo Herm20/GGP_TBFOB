a04856d096bf41b58001f1edadb906036369a18b
Ollyting Xin (RIT Student) <ox7313@ad.rit.edu>
2019-03-04 16:22:49 -0500

    imported assets

---
+++ GGP_Prototype/Assets/Scripts/Spell.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spell : MonoBehaviour {
+
+    public GameObject spellPrefab;
+    public float power;
+	// Update is called once per frame
+	void FixedUpdate () {
+        if (Input.GetMouseButtonDown(0)) {
+            //Debug.Log("Shoooot");
+            GameObject spellObj = Instantiate(spellPrefab, this.transform.position + this.transform.forward, Quaternion.identity) as GameObject;
+            spellObj.GetComponent<Rigidbody>().AddForce(spellObj.transform.forward * power);
+        }
+	}
+}
